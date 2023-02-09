4c771420a9a307d3ea654bc836ffce50de8e76d9
Valeria Tyan <vlrtyan@yandex.ru>
2023-02-10 00:44:11 +0300

    banners

---
+++ README.md
@@ -0,0 +1,9 @@
+# Тестовое задание для Go Ahead
+
+## Инструменты
+
+1. HTML
+2. CSS
+3. JS
+
+## Перейти на сайт можно по [ссылке](https://vlrtyan.github.io/goahead-test)
---
+++ pages/index.js
@@ -0,0 +1,15 @@
+let slideIndex = 0;
+showSlides();
+function showSlides() {
+  let i;
+  let slides = Array.from(document.querySelectorAll(".banner"));
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {
+    slideIndex = 1;
+  }
+  slides[slideIndex - 1].style.display = "flex";
+  setTimeout(showSlides, 4000);
+}
