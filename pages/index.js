0b781028cba7ea72436c0880e13f68fe66f5f7c9
Valeria Tyan <vlrtyan@yandex.ru>
2023-02-10 17:06:52 +0300

    fix animation for ios

--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,17 @@
 let slideIndex = 0;
-showSlides();
 function showSlides() {
   let i;
   let slides = Array.from(document.querySelectorAll(".banner"));
   for (i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";
-    slides[i].style.animation= "none";
+    
   }
   slideIndex++;
   if (slideIndex > slides.length) {
     slideIndex = 1;
   }
-  slides[slideIndex - 1].style.animation= "display 4s infinite";
   slides[slideIndex - 1].style.display = "flex";
   setTimeout(showSlides, 4000);
 }
+
+showSlides();
